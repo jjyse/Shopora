b2ac2c9a8e13abc2075eea1681735875979da698
Joyce S <joycejseok@gmail.com>
2021-06-07 21:01:38 +0100

    sdfsdf

--- private/secret_settings.py
+++
@@ -1,3 +0,0 @@
-# single sign on Google (will be used if provided)
-OAUTH2GOOGLE_CLIENT_ID = '485357230404-bcbdptrhkl602a3k0hs0ks0ssf9u99r2.apps.googleusercontent.com'
-OAUTH2GOOGLE_CLIENT_SECRET = 'Dk-QRB9HukWgyU7jiVnPC5J9'
